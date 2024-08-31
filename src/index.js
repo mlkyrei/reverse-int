ef088d8f8f1d9a4c23e6866610fe7fbb226fb973
Malika Bukenova <mb.bukenova@gmail.com>
2024-09-01 01:01:12 +0500

    fix: add a code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let results = '';
+    let str = n.toString();
+    for (let i = 1; i <= str.length; i++) {
+        results += str[str.length - i];
+    }
+  return Number(results);
 }
