d1811d82a604f042389aa45d9bab127a073783a9
Malika Bukenova <mb.bukenova@gmail.com>
2024-09-01 01:12:50 +0500

    fix: fix a string

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = function reverse (n) {
     let a = Math.abs(n);
     let results = '';
     let str = a.toString();
-    for (let i = 1; i <= a.length; i++) {
-        results += str[a.length - i];
+    for (let i = 1; i <= str.length; i++) {
+        results += str[str.length - i];
     }
   return Number(results);
 }
